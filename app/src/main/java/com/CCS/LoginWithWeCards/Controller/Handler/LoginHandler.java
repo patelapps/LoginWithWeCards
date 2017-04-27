e9d80e0c9ffdc5b2b8992de5dbd1770bc57ecd4b
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2017-04-27 16:45:27 +0530

    new change

--- app/src/main/java/com/CCS/LoginWithWeCards/Controller/Handler/LoginHandler.java
+++ app/src/main/java/com/CCS/LoginWithWeCards/Controller/Handler/LoginHandler.java
@@ -1,9 +1,11 @@
 package com.CCS.LoginWithWeCards.Controller.Handler;
 
+import android.support.annotation.Keep;
+
 /**
  * Created by mauliksantoki on 24/4/17.
  */
-
+@Keep
 public interface LoginHandler {
 
     public void result(String Result);
