32c265e373b905ea415d97d2ce00aefed593346b
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2017-04-28 16:26:53 +0530

    new change

--- app/src/main/java/com/CCS/LoginWithWeCards/Utils/appInstalledOrNot.java
+++ app/src/main/java/com/CCS/LoginWithWeCards/Utils/appInstalledOrNot.java
@@ -1,6 +1,7 @@
 package com.CCS.LoginWithWeCards.Utils;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
@@ -14,8 +15,11 @@ public class appInstalledOrNot {
 
     public static boolean appInstalledOrNot(String packagename, Context context) {
         try {
-            context.getPackageManager().getPackageInfo(packagename, 0);
-            return true;
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(packagename, 0);
+            /*we add login with wecards in version code 5 ,version 1.0.4*/
+            if (packageInfo.versionCode > 4) {
+                return true;
+            }
         } catch (PackageManager.NameNotFoundException e) {
             Log.e(AppContacts.Tag, e.getMessage());
 
