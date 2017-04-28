184f9454ff63575f11a2f8f07577ecf8fd5c78fd
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2017-04-28 19:14:37 +0530

    new change

--- app/src/main/java/com/CCS/LoginWithWeCards/UI/LoginScreen.java
+++ app/src/main/java/com/CCS/LoginWithWeCards/UI/LoginScreen.java
@@ -141,9 +141,14 @@ public class LoginScreen extends Dialog implements ScreenHandler {
         Window window = getWindow();
         window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
 //        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, (int) (displayRectangle.height() * 0.95f));
-        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+
+        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         setCanceledOnTouchOutside(false);
+
+        ViewGroup.LayoutParams layoutParams = llLogin.getLayoutParams();
+        layoutParams.height = (int) (displayRectangle.height() * 0.95f);
+        llLogin.setLayoutParams(layoutParams);
 
     }
 
