b17fc45cd38bdc1d892dc6186688f6311ae1012e
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2017-04-28 18:51:13 +0530

    new change

--- app/src/main/java/com/CCS/LoginWithWeCards/CustomView/DialogProgress.java
+++ app/src/main/java/com/CCS/LoginWithWeCards/CustomView/DialogProgress.java
@@ -1,10 +1,16 @@
 package com.CCS.LoginWithWeCards.CustomView;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Build;
+import android.text.SpannableString;
+import android.text.Spanned;
 
 import com.CCS.LoginWithWeCards.R;
+import com.CCS.LoginWithWeCards.Utils.AppTypeface;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyTypefaceSpan;
 
 
 /**
@@ -12,11 +18,19 @@ import com.CCS.LoginWithWeCards.R;
  */
 
 public class DialogProgress extends ProgressDialog {
+    AppTypeface typeface;
 
-
-    public DialogProgress(Context context) {
+    public DialogProgress(Activity context) {
         super(context, getStyle());
-        setMessage("Please Wait ...");
+
+        typeface = new AppTypeface(context);
+        setCancelable(false);
+
+        String message = "Please Wait ...";
+        SpannableString spannableString = new SpannableString(message);
+        CalligraphyTypefaceSpan typefaceSpan = new CalligraphyTypefaceSpan(typeface.getRegularFont());
+        spannableString.setSpan(typefaceSpan, 0, message.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        setMessage(spannableString);
         setCancelable(false);
     }
 
