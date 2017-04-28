cc71690d486c3267e06d35a4c665750b5c9ee5bc
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2017-04-28 17:56:26 +0530

    new change

--- app/src/main/java/com/CCS/LoginWithWeCards/Controller/Handler/LoginHandler.java
+++ app/src/main/java/com/CCS/LoginWithWeCards/Controller/Handler/LoginHandler.java
@@ -1,13 +1,12 @@
 package com.CCS.LoginWithWeCards.Controller.Handler;
 
-import android.os.Parcelable;
 import android.support.annotation.Keep;
 
 /**
  * Created by mauliksantoki on 24/4/17.
  */
 @Keep
-public interface LoginHandler extends Parcelable {
+public interface LoginHandler {
 
     public void loginResult(String Result);
 
