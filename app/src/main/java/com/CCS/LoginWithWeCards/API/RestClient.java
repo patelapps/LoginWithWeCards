dae096a1b943c938ea1e47844718058e3006a9d9
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2018-12-10 21:32:18 +0530

    Bug Fixing

--- app/src/main/java/com/CCS/LoginWithWeCards/API/RestClient.java
+++ app/src/main/java/com/CCS/LoginWithWeCards/API/RestClient.java
@@ -1,37 +1,43 @@
 package com.CCS.LoginWithWeCards.API;
 
+
 import com.CCS.LoginWithWeCards.Model.LoginRequest;
 import com.CCS.LoginWithWeCards.Model.LogoutRequest;
 
+import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 
 public class RestClient {
 
     private static GitApiInterface gitApiInterface;
+
     public static GitApiInterface getClient() {
         if (gitApiInterface == null) {
             Retrofit client = new Retrofit.Builder()
-                    .baseUrl(urls.LIVE_BASIC_URL)
+                    .baseUrl(urls.LIVE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
 
 
             gitApiInterface = client.create(GitApiInterface.class);
         }
+
         return gitApiInterface;
     }
 
     public interface GitApiInterface {
-        @POST("open/user/login")
+        @POST("?r=" + urls.API_VERSION + "open/user/login")
         Call<ResponseBody> login(@Body LoginRequest body);
 
-        @POST(" open/user/logout")
+        @POST("?r=" + urls.API_VERSION + "open/user/logout")
         Call<ResponseBody> logout(@Body LogoutRequest body);
     }
 }
