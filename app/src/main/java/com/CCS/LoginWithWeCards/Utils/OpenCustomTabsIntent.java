71848e96e2a816dd20724daa5d2a5de04cc9e98b
sagar <sagarpatel288@gmail.com>
2018-12-13 15:00:30 +0530

    --1-- Material Design, Single Page, Native Error Dialog.

---
+++ app/src/main/java/com/CCS/LoginWithWeCards/Utils/OpenCustomTabsIntent.java
@@ -0,0 +1,71 @@
+package com.skeletonlibrary.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+import com.skeletonlibrary.R;
+
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.core.content.ContextCompat;
+
+
+/**
+ * Created by mauliksantoki on 20/6/17.
+ */
+
+public final class OpenCustomTabsIntent {
+
+    private OpenCustomTabsIntent() {
+    }
+
+    public static void openCustomTabsIntent(Activity activity, String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(activity, R.color.ThemeColor));
+        CustomTabsIntent customTabsIntent = builder.build();
+        if (appInstalledOrNot(activity, "com.android.chrome")) {
+            customTabsIntent.intent.setPackage("com.android.chrome");
+        }
+        customTabsIntent.launchUrl(activity, Uri.parse(url));
+    }
+
+    public static void openCustomTabsIntent(Context activity, String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(activity, R.color.ThemeColor));
+        CustomTabsIntent customTabsIntent = builder.build();
+        if (appInstalledOrNot(activity, "com.android.chrome")) {
+            customTabsIntent.intent.setPackage("com.android.chrome");
+        }
+        customTabsIntent.launchUrl(activity, Uri.parse(url));
+    }
+
+    public static boolean appInstalledOrNot(Activity activity, String packageName) {
+        PackageManager pm = activity.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            ApplicationInfo ai = activity.getPackageManager().getApplicationInfo(packageName, 0);
+            if (ai.enabled) {
+                return true;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        return false;
+    }
+
+    public static boolean appInstalledOrNot(Context activity, String packageName) {
+        PackageManager pm = activity.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            ApplicationInfo ai = activity.getPackageManager().getApplicationInfo(packageName, 0);
+            if (ai.enabled) {
+                return true;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        return false;
+    }
+}
